baa2e7e3305d50851942541cc8c0e0168c92a29e
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-26 13:34:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchie][:languages] = "C"
+    programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 p dennis_ritchies_language
